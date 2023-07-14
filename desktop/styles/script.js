5b97df1689cdf0dfc2718bcdb60e84f58f743714
4194304 <stereomadnessss@protonmail.com>
2023-07-14 12:48:02 -0700

    Desktop interface prototype
    JS

---
+++ desktop/styles/script.js
@@ -0,0 +1,77 @@
+function close1() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window')
+      .style.display = "none";
+  document.getElementById('windowheader')
+    .style.display = "none";
+}
+function close2() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window2')
+      .style.display = "none";
+  document.getElementById('windowheader2')
+    .style.display = "none";
+}
+function close3() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window3')
+      .style.display = "none";
+  document.getElementById('windowheader3')
+    .style.display = "none";
+}
+function close4() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window5')
+      .style.display = "none";
+  document.getElementById('windowheader5')
+    .style.display = "none";
+}
+
+function show() {
+    /* Access image by id and change
+    the display property to block*/
+    document.getElementById('menu')
+        .style.display = "flex";
+}
+function window1() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window')
+      .style.display = "block";
+  document.getElementById('windowheader')
+    .style.display = "flex";
+}
+function window2() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window2')
+      .style.display = "block";
+  document.getElementById('windowheader2')
+    .style.display = "flex";
+}
+function window3() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window3')
+      .style.display = "block";
+  document.getElementById('windowheader3')
+    .style.display = "flex";
+}
+function hide() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('menu')
+      .style.display = "none";
+}
+function window4() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window5')
+      .style.display = "block";
+  document.getElementById('windowheader5')
+    .style.display = "flex";
+}
