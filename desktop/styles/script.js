07f57caf674e543cc6263827418eace2ababb88f
4194304 <stereomadnessss@protonmail.com>
2023-07-14 16:26:14 -0700

    Desktop V2
    With games and some UI enhancements

--- desktop/styles/script.js
+++ desktop/styles/script.js
@@ -30,13 +30,6 @@ function close4() {
   document.getElementById('windowheader5')
     .style.display = "none";
 }
-
-function show() {
-    /* Access image by id and change
-    the display property to block*/
-    document.getElementById('menu')
-        .style.display = "flex";
-}
 function window1() {
   /* Access image by id and change
   the display property to block*/
@@ -61,12 +54,6 @@ function window3() {
   document.getElementById('windowheader3')
     .style.display = "flex";
 }
-function hide() {
-  /* Access image by id and change
-  the display property to block*/
-  document.getElementById('menu')
-      .style.display = "none";
-}
 function window4() {
   /* Access image by id and change
   the display property to block*/
@@ -74,4 +61,143 @@ function window4() {
       .style.display = "block";
   document.getElementById('windowheader5')
     .style.display = "flex";
+}
+function exit() {
+  close()
+}
+function window6() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window6')
+      .style.display = "block";
+  document.getElementById('windowheader6')
+    .style.display = "flex";
+}
+function close6() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window6')
+      .style.display = "none";
+  document.getElementById('windowheader6')
+    .style.display = "none";
+}
+function close7() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window7')
+      .style.display = "none";
+  document.getElementById('windowheader7')
+    .style.display = "none";
+}
+function window7() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window7')
+      .style.display = "block";
+  document.getElementById('windowheader7')
+    .style.display = "flex";
+}
+function window8() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window8')
+      .style.display = "block";
+  document.getElementById('windowheader8')
+    .style.display = "flex";
+}
+function close8() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window8')
+      .style.display = "none";
+  document.getElementById('windowheader8')
+    .style.display = "none";
+}
+function close9() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window9')
+      .style.display = "none";
+  document.getElementById('windowheader9')
+    .style.display = "none";
+}
+function window9() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window9')
+      .style.display = "block";
+  document.getElementById('windowheader9')
+    .style.display = "flex";
+}
+function close10() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window10')
+      .style.display = "none";
+  document.getElementById('windowheader10')
+    .style.display = "none";
+}
+function window10() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window10')
+      .style.display = "block";
+  document.getElementById('windowheader10')
+    .style.display = "flex";
+}
+function jezzball() {
+var url = data.url + "&output=embed";
+window.location.replace(url);
+}
+function catpics() {
+  document.getElementById('downarrowcat')
+  .style.display = "none";
+  document.getElementById('uparrowcat')
+  .style.display = "block";
+document.getElementById('catpics')
+.style.display = "flex";
+}
+function hidepics() {
+  document.getElementById('uparrowcat')
+  .style.display = "none";
+  document.getElementById('downarrowcat')
+  .style.display = "block";
+document.getElementById('catpics')
+.style.display = "none";
+}
+function lazyclose() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window11')
+      .style.display = "none";
+  document.getElementById('windowheader11')
+    .style.display = "none";
+}
+function lazy() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('window11')
+      .style.display = "block";
+  document.getElementById('windowheader11')
+    .style.display = "flex";
+}
+function show() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('menu')
+      .style.display = "flex";
+  document.getElementById('btnID')
+    .style.display = "none";
+    document.getElementById('starthide')
+    .style.display = "block";
+}
+function hide() {
+  /* Access image by id and change
+  the display property to block*/
+  document.getElementById('menu')
+      .style.display = "none";
+  document.getElementById('btnID')
+    .style.display = "block";
+    document.getElementById('starthide')
+    .style.display = "none";
 }
