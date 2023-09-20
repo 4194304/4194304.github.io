9b8610dbc29f10b024615b05819aa46343dc67d5
4194304 <130800536+4194304@users.noreply.github.com>
2023-09-20 07:51:46 -0700

    Add files via upload

---
+++ versionarchive/script.js
@@ -0,0 +1,40 @@
+function welcome() {
+    document.getElementById('main')
+    .style.display = "inline-block";
+    document.getElementById('bit32')
+    .style.display = "none";
+    document.getElementById('bit64')
+    .style.display = "none";
+    document.getElementById('bigint')
+    .style.display = "none";
+}  
+function bit32() {
+    document.getElementById('main')
+    .style.display = "none";
+    document.getElementById('bit32')
+    .style.display = "inline-block";
+    document.getElementById('bit64')
+    .style.display = "none";
+    document.getElementById('bigint')
+    .style.display = "none";
+} 
+function bit64() {
+    document.getElementById('main')
+    .style.display = "none";
+    document.getElementById('bit32')
+    .style.display = "none";
+    document.getElementById('bit64')
+    .style.display = "inline-block";
+    document.getElementById('bigint')
+    .style.display = "none";
+}  
+function bigint() {
+    document.getElementById('main')
+    .style.display = "none";
+    document.getElementById('bit32')
+    .style.display = "none";
+    document.getElementById('bit64')
+    .style.display = "none";
+    document.getElementById('bigint')
+    .style.display = "inline-block";
+} 
